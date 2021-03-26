c041f93af35f1a4dbcc05555e9de3987e4e5578d
terenyicsaba <terenyicsaba@gmail.com>
2021-03-26 13:09:15 +0100

    Words commitja

--- src/main/java/hu/nive/ujratervezes/zarovizsga/Words.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/Words.java
@@ -1,6 +1,18 @@
 package hu.nive.ujratervezes.zarovizsga;
 
 public class Words {
+
     public boolean hasMoreDigits(String s) {
+           int countDigits = 0;
+           int countOther = 0;
+
+           for(char c : s.toCharArray()){
+               if(Character.isDigit(c)){
+                   countDigits++;
+               }else{
+                   countOther++;
+               }
+           }
+    return countDigits-countOther>0;
     }
 }
